274c8b92d2093d4ac0c43577d368901754c50e06
Abdul Manaf <abdulmanaf.mscss24@iba-suk.edu.pk>
2025-07-13 19:14:05 +0500

    updates

--- Model.py
+++ Model.py
@@ -1,5 +1,4 @@
 from tensorflow.keras import layers, models
-import tensorflow as tf
 import numpy as np
 import cv2
 
@@ -16,13 +15,13 @@ class MyModel:
         self.model.add(layers.Flatten())
         self.model.add(layers.Dense(1, activation='sigmoid'))
         self.model.compile(loss="binary_crossentropy", optimizer='adam', metrics=["accuracy"])
-        self.model.load_weights('static/weights.h5')
+        self.model.load_weights('./static/weights.h5')
 
-    def predict(self, img_path):
-        i = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    def predict(self):
+        i = cv2.imread('./static/image.png', cv2.IMREAD_GRAYSCALE)
         i = cv2.resize(i, (self.IMAGE_SIZE, self.IMAGE_SIZE))
         i = np.array(i).reshape(-1, self.IMAGE_SIZE, self.IMAGE_SIZE, 1)
         i = np.array(i).astype('float')/255.0
         prediction = self.model.predict([i])
-        return (['NORMAL', 'PNEUMONIA'][int(prediction.round())], prediction[0][0])
+        return ((['NORMAL', 'PNEUMONIA'][int(prediction.round())], prediction[0][0]))
 
--- main.py
+++ main.py
@@ -1,44 +1,28 @@
-import os
-import numpy as np
 import Model
-from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request
 
-UPLOAD_FOLDER = './static\\model_data\\'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = './static/'
 model = Model.MyModel()
-
-
-def DeleteTemporaryFiles():
-    files = os.scandir('./static/model_data')
-    for file in files:
-        if file.is_file():
-            os.remove(file)
-
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=["GET", "POST"])
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
-        DeleteTemporaryFiles()
         file = request.files['image']
-        fileName = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
-        file.save(fileName)
-        (name, prob) = model.predict(fileName)
+        file.save('./static/image.png')
+        (name, prob) = model.predict()
         print(name, prob)
         if name == 'NORMAL':
             prob = f'The probabilty of having no Pneumonia is {str(1-prob)[:5]}%'
         elif name == 'PNEUMONIA':
             prob = f'The probabilty of having Pneumonia is {str(prob)[:5]}%'
 
-        return render_template("results.html", predicted_name=name, probability=prob, url=fileName)
-    return render_template('index.html')
+        return render_template("results.html", predicted_name=name, probability=prob)
 
 
 app.run(debug=True)
